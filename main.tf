a72651104971bd30ab3ac387515d044399a1f220
Brijesh Raman <brijesh.zte@gmail.com>
2025-07-06 17:10:06 +0530

    brijusubnetadded

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "rg1" {
     name = "ramanrgnew"
     location = "eastus"
 }
+resource "azurerm_subnet" "subnet" {
+    name = "birjusubnet"
+    virtual_network_name = "birjvnet"
+    resource_group_name = "ramanrgnew"
+    address_prefixes = ["10.0.0.0/24"]
+}
